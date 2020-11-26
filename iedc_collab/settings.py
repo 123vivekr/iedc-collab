36d41190e21554a0db6f5bca71229b513797a1e4
Ayush Singh <ayu.52056@gmail.com>
2020-11-26 16:02:55 +0530

    Added Both Phone Number Validator and get time automatically feature

--- iedc_collab/settings.py
+++ iedc_collab/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,20 +75,14 @@ WSGI_APPLICATION = 'iedc_collab.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'collab_platform',
-#         'USER': 'root',
-#         'PASSWORD': 'collab123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'collab_platform',
+        'USER': 'root',
+        'PASSWORD': 'collab123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
